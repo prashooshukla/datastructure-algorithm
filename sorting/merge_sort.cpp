4e016b094d20371633ad5034014df4d6379b3088
prashooshukla <prashooshukla9@gmail.com>
2020-06-22 01:00:33 +0530

    merge sort reverse implementation

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // one from start to mid
 // second from mid+1, to end
 // and merge them in sorted order
-void merge(vector<int> &arr, int start, int mid, int end) {
+void merge(vector<int> &arr, int start, int mid, int end, char c = 's') {
     vector<int> temp(end - start + 1); // create a temp array
 
     // crawler for both interval
@@ -14,14 +14,24 @@ void merge(vector<int> &arr, int start, int mid, int end) {
     int j = mid + 1;
     int k = 0;
 
-    // traverse both array and add smaleer of both in temp array
+    // traverse both array and add smaller of both in temp array
     while (i <= mid and j <= end) {
-        if (arr[i] <= arr[j]) {
-            temp[k] = arr[i];
-            ++k; ++i;
+        if (c == 'g') {
+            if (arr[i] >= arr[j]) {
+                temp[k] = arr[i];
+                ++k; ++i;
+            } else {
+                temp[k] = arr[j];
+                ++k; ++j;
+            }
         } else {
-            temp[k] = arr[j];
-            ++k; ++j;
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                ++k; ++i;
+            } else {
+                temp[k] = arr[j];
+                ++k; ++j;
+            }   
         }
     }
 
@@ -33,7 +43,7 @@ void merge(vector<int> &arr, int start, int mid, int end) {
 
     // adding remaining elements in second interval
     while (j <= end) {
-        temp[k] = arr[i];
+        temp[k] = arr[j];
         ++k; ++j;
     }
 
@@ -44,12 +54,14 @@ void merge(vector<int> &arr, int start, int mid, int end) {
 }
 
 // recursive implementation of merge sort
-void merge_sort(vector<int> &arr, int start, int end) {    
+// default agrument decides ascending/descending order
+void merge_sort(vector<int> &arr, int start, int end, char c = 's') {    
+   
     if (start < end) {
         int mid = (start + end) / 2;
-        merge_sort(arr, start, mid);
-        merge_sort(arr, mid + 1, end);
-        merge(arr, start, mid, end);
+        merge_sort(arr, start, mid, c);
+        merge_sort(arr, mid + 1, end, c);
+        merge(arr, start, mid, end, c);
     }
 }
 
@@ -64,12 +76,19 @@ signed main() {
 
     vector<int> arr = {5, 7, 1, 3, 6, 4, 2, 8};
     int n = arr.size();
-
     cout << "before sorting: ";
     print(arr);
     merge_sort(arr, 0, n - 1);
     cout << "after sorting: ";
     print(arr);
 
+    arr = {5, 14, 7, 10, 9, 6, 4, 20, 18};
+    n = arr.size();
+    cout << "before reverse sorting: ";
+    print(arr);
+    merge_sort(arr, 0, n - 1, 'g'); // argument for sorting in reverse.
+    cout << "after reverese sorting ";
+    print(arr);
+    
     return 0;
 }
