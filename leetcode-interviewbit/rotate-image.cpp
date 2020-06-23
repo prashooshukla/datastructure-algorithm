e6b47ef8f7fb19ddb4817a4d37ce85d93e4916d0
prashooshukla <prashooshukla9@gmail.com>
2020-06-23 21:43:20 +0530

    9:43 PM

---
+++ leetcode-interviewbit/rotate-image.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/rotate-image/submissions/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        void rotate(vector<vector<int>>& matrix);
+};
+
+void Solution::rotate(vector<vector<int>>& matrix) {
+    int N = matrix.size();
+    // transponse the matrix
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i < j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+    // reverse each row using two pointers technique
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N / 2; j++) {
+            swap(matrix[i][j], matrix[i][N - 1 - j]);
+        }
+    }
+}
