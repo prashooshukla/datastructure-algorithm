8a5b0c90255cd4c674382fe99bbd0e20f15114c4
prashooshukla <prashooshukla9@gmail.com>
2020-07-21 01:59:02 +0530

    MS Amazon

---
+++ Stack-Queue/nearest-smaller-element.cpp
@@ -0,0 +1,22 @@
+// https://www.interviewbit.com/problems/nearest-smaller-element/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> prevSmaller(vector<int>&);
+};
+
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    int N = (int) A.size();
+    stack<int> S;
+    vector<int> sol(N);
+    for (int i = 0; i < N; i++) {
+        while (!S.empty() and A[i] <= S.top()) S.pop();
+        if (S.empty()) sol[i] = -1;
+        else sol[i] = S.top();
+        S.push(A[i]);
+    }
+    return sol;
+}
